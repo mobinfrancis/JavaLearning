a9a9b85e8e7c0aee0eb254670ff1b68a35050b88
MOBIN FRANCIS <MOBIN FRANCIS@BLESSINGS-PC>
2019-07-15 22:41:07 +0530

    Object and Class

---
+++ CoreJavaLearning/src/OOPConcept1/Car.java
@@ -0,0 +1,21 @@
+package OOPConcept1;
+
+public class Car {
+	int mod;
+	int wheel;
+
+	public static void main(String[] args) {
+//	int mod;
+//	int wheel;    wont work; a.mod 
+	
+	Car a =new Car();
+	a.mod=123;
+	a.wheel=3;
+	
+	System.out.println(a.mod);
+	System.out.println(a.wheel);
+	
+
+	}
+
+}
