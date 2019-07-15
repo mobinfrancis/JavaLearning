183abca769a5d7da6b3fc19195a117a54d5a1271
MOBIN FRANCIS <MOBIN FRANCIS@BLESSINGS-PC>
2019-07-15 09:45:15 +0530

    Loops

---
+++ CoreJavaLearning/src/Basics/whileLooptest.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class whileLooptest {
+	
+
+	public static void main(String[] args) {
+		
+		int i =0;
+		int j = i++;
+		System.out.println("hello");
+	
+		while(i<5)
+		{
+			//System.out.println(i);
+			i=i+1;
+		}
+		System.out.println(j);
+		System.out.println(i);
+	}
+
+}
--- CoreJavaLearning/src/sample/SampleJava.java
+++ CoreJavaLearning/src/sample/SampleJava.java
@@ -3,8 +3,40 @@ package sample;
 public class SampleJava {
 
 	public static void main(String[] args) {
-		System.out.println("I have written my first java program");
 
+		
+		System.out.println("I have written my first java program");
+		
+		// Post Increment
+int i =9;
+int j =i++;
+
+System.out.println(i);
+System.out.println(j);
+	
+
+	
+	//Pre Increment
+int a =12;
+int b = ++a;
+	
+	System.out.println(a);
+	System.out.println(b);
+	
+	//Post Decrement
+	
+int x= 20;
+int y=x--;
+System.out.println(x);
+System.out.println(y);
+
+
+// //Pre Decrement
+
+int p =10;
+int q =--p;
+System.out.println(p);
+System.out.println(q);
 	}
 
 }
