54f12e79604bccbba21eca519eb26e54e856cc98
mobin.francis <mobin.francis@BDC4-D-48J7N62.dir.svc.accenture.com>
2019-07-26 17:01:12 +0530

    throws keyword

--- CoreJavaLearning/src/ThrowsKeyword.java
+++ CoreJavaLearning/src/ThrowsKeyword.java
@@ -1,9 +1,26 @@
 
 public class ThrowsKeyword {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws ArithmeticException {
+		
+		ThrowsKeyword obj = new ThrowsKeyword();
+		obj.sum();
 
+
+
+	}
+	
+	public void sum()throws ArithmeticException
+	{
+		div();
+	}
+	
+	public void div() throws ArithmeticException
+	
+	try{
+	{
+		int i =9/0;
 	}
 
 }
+	catch(ArithmeticException e)
