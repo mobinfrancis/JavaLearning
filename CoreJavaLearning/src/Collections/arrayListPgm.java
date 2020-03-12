e3e08ab189ebcd5b4d6ae44d4ed3c8427d167001
mobin <mobinfrancis@live.com>
2020-03-12 17:08:30 +0530

    added arraylist pgm

---
+++ CoreJavaLearning/src/Collections/arrayListPgm.java
@@ -0,0 +1,21 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class arrayListPgm {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Mobin");
+		al.add("Monisha");
+		al.add("Annu");
+		al.add("Kunju");
+		
+		System.out.println("------CONTENTS IN ARRAYLIST----");
+		System.out.println(al);
+		//Can use contains directly in the arraylist
+		System.out.println(al.contains("Francis"));
+
+	}
+
+}
